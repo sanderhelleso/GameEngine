c805752a504cc356517278ceee4b3678a7fd22f5
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-03 18:27:29 -0700

    added renderer

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,13 +13,36 @@ public class MainGameLoop {
         // create main game display
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        // openGL expects verticles to be defined counter clockwise by default
+        float[] vericles = {
+                // left bottom triangle
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+
+                // right top triangle
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f,
+        };
+
+        RawModel model = loader.loadToVAO(vericles);
+
         // run until window is closed
         while(!Display.isCloseRequested()) {
+            renderer.prepare();
 
             // game logic
+            renderer.render(model);
+
             //render
             DisplayManager.updateDisplay();
         }
+
+        loader.cleanUp();
 
         // close window and process after window is closed
         DisplayManager.closeDisplay();
